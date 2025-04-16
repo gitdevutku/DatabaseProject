c0619d481eab9521a5836587ebdcd09a0e5af1dc
Utku Berki Baysal <utkuberki21@gmail.com>
2025-04-16 20:20:18 +0300

    Add project files.

---
+++ DatabaseCrud/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Read Update Delete
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------");
+            Console.Write("Eklemek İstediğiniz Kategori Adı: ");
+            string categoryName = Console.ReadLine();
+
+            SqlConnection sqlConnection = new SqlConnection("Data Source = UTKU\\SQLEXPRESS ; initial catalog=EgitimKampiDb;integrated security=true");
+            sqlConnection.Open();
+
+            SqlCommand cmd = new SqlCommand("insert into TblCategory (CategoryName) values (@p1)", sqlConnection);
+            cmd.Parameters.AddWithValue("@p1", categoryName);
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            Console.WriteLine("")
+        }
+    }
+}
---
+++ DatabaseProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi ******");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            string tableNumber = Console.ReadLine();
+            Console.WriteLine("--------------------------");
+
+
+            SqlConnection sqlConnection = new SqlConnection("Data Source=UTKU\\SQLEXPRESS;initial Catalog=EgitimKampiDb;integrated security=true");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("Select * from TblCategory", sqlConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            sqlConnection.Close();
+            foreach (DataRow item in dataTable.Rows)
+            {
+                foreach (var row in item.ItemArray)
+                {
+                    Console.Write(row.ToString());
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
